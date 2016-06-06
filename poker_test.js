b6cd26ef8545b4eb7eafd02ed292eefa49a35b81
Steve <steve@tripgrid.com>
2016-06-05 22:30:13 -0700

    added helper functions for sorting arrays, refactored some code in is_flush

--- poker_test.js
+++ poker_test.js
@@ -23,12 +23,7 @@ function is_flush(hand) {
     //separate suit for flush/straight flush/royal possibilities
     suits.push(hand[i].charAt(0));
   }
-  suits.sort();
-  if (suits[0] == suits[suits.length - 1]) {
-    return true
-  } else {
-    return false
-  }
+  return suits[0] == suits[suits.length - 1]
 }
 
 function is_num_hand(hand) {
@@ -40,15 +35,14 @@ function is_num_hand(hand) {
   }
   values.sort(sortAsc);
 
-/*
-  if (values[0] === values[3]) || (value[1] === values[4]) {
+  if (values[0] === values[3] || values[1] === values[4]) {
     //confirms 4 of a kind by seeing if 1st and 4th value match, or 2 and 5th value match
     return [4, values[2]]
-  } else if (values[0] === values[2]) || (values[1] === values[3]) || (values[2] === values[4]) {
+  } else if (values[0] === values[2] || values[1] === values[3] || values[2] === values[4]) {
     //confirms 3 of a kind by checking if 1st and 3rd, 2nd and 4th, or 3rd and 5th cards match
     return [3, values[2]]
   }
-*/
+
 }
 
 function sortAsc(a, b) {return a-b};
